f6995fcf0df7eccd557edab5c7332240a24f47d5
Zakhar Zakharchuk <zakhar.zakharchuk3@gmail.com>
2022-12-14 19:23:10 +0200

    repositories

--- backend/src/main/java/com/example/backend/repository/FacilityRepository.java
+++ backend/src/main/java/com/example/backend/repository/FacilityRepository.java
@@ -2,12 +2,16 @@ package com.example.backend.repository;
 
 import com.example.backend.entity.Facility;
 import java.util.List;
+import java.util.Optional;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface FacilityRepository extends CrudRepository<Facility, Long> {
     List<Facility> findAll();
+
+    Optional<Facility> findById(Long facilityId);
+
     Facility save(Facility facility);
     void deleteById(Long facilityId);
 }
