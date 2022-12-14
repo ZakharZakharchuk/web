8841a502a4bd7e38869cfc6e3b7a9db836a9534b
KSYliD <ia14.kondrasiuk.yaroslav@gmail.com>
2022-12-14 23:25:12 +0200

    1 st commit

---
+++ frontend/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+<div>
+  <HeaderComponent/>
+
+  <main>
+    <router-view />
+  </main>
+
+  <FooterComponent/>
+</div>
+</template>
+
+<script>
+
+import HeaderComponent from "@/components/layouts/HeaderComponent";
+import FooterComponent from "@/components/layouts/FooterComponent";
+export default {
+  name: 'App',
+  components: {
+    FooterComponent,
+    HeaderComponent
+  }
+}
+</script>
+
+<style>
+
+</style>
